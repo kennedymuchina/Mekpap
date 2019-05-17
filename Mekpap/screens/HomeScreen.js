c416e91a8ded47f5be2238cd6a9c3d01ad095574
Kennedy Muchina <kennedymuchina@gmail.com>
2019-05-17 12:22:36 +0300

    first pages and some backend

--- Mekpap/screens/HomeScreen.js
+++ Mekpap/screens/HomeScreen.js
@@ -5,97 +5,84 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
-import { WebBrowser } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+import { LinearGradient, Font } from 'expo';
 
 export default class HomeScreen extends React.Component {
+  state = {
+    fontLoaded: false
+  }
   static navigationOptions = {
     header: null,
   };
+  async componentDidMount() {
+    await Font.loadAsync({
+      'raleway': require('../assets/fonts/Raleway-Light.ttf'),
+    });
+
+    this.setState({fontLoaded: true})
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
+          <LinearGradient
+             colors={['rgba(0,72,111,1)', 'rgba(42,136,136,0.8)']}
+             start={{x: 0, y: 0.5}} 
+             end={{x: 1, y: 0.5}}
+             style={{
+               flex: 1,
+               justifyContent: 'center',
+               paddingTop: 50,
+             }}  
+          >
+          <ScrollView>
+            <Text
+              style={{
+                marginVertical: 10,
+                fontSize: 34,
+                color: '#fff',
+                fontWeight: '100',
+                textAlign: 'center',
+                // fontFamily: 'raleway',
+              }} 
+            >
+              Nice to see you back,
             </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
+            <Text
+              style={{
+                marginVertical: 50,
+                fontSize: 40,
+                color: '#fff',
+                fontWeight: '100',
+                textAlign: 'center',
+                // fontFamily: 'raleway',
+              }} 
+            >
+              Kennedy
+            </Text>
+            <TextInput
+              style={{
+                height: 40,
+                borderColor: 'white', 
+                borderWidth: 1,
+                paddingHorizontal: 10, 
+                marginLeft: 30,
+                borderRadius: 7, 
+                marginRight: 30}}
+              // onChangeText={(text) => this.setState({text})}
+              // value={this.state.text}
+                placeholder = "Eg. Car Won't Start"
+            />
+          </ScrollView>
+          </LinearGradient>
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
@@ -103,44 +90,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
+    justifyContent: 'center',
   },
   getStartedText: {
+    alignItems: 'center',
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
@@ -170,19 +126,5 @@ const styles = StyleSheet.create({
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
     textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
